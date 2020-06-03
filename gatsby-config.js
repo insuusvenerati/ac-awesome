657c861630aaddb5f9952cccc5eda2d7e0e0d9b9
Sean Norwood <norwood.sean@gmail.com>
2020-06-03 15:38:52 -0500

    chore: some updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,23 +3,11 @@ module.exports = {
     `gatsby-plugin-postcss`,
     `gatsby-plugin-manifest`,
     `gatsby-plugin-preact`,
-    {
-      resolve: `gatsby-plugin-webpack-bundle-analyzer`,
-      options: {
-        production: true,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-scroll-reveal`,
-      options: {
-        threshold: 0.1,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-offline`,
-      options: {
-        precachePages: [`/index/`],
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-offline`,
+    //   options: {
+    //     precachePages: [`/index/`],
+    //   },
+    // },
   ],
 };
